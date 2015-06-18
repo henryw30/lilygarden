f5de6cd2175db8a2189d176c2feeb66782b79331
Henry <henrywu847@bitbucket.org>
2015-06-18 15:10:19 -0500

    button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,10 @@ class ApplicationController < ActionController::Base
   	file_as_string = file.read
   	@json_hash = JSON.parse(file_as_string)
   	@menu_hash = @json_hash['menu']
+    @restaurant_name = @json_hash['restaurant']
+    @address_hash = @json_hash['address']
+    @phone = @json_hash['phone']
+    @hours_hash = @json_hash['hours']
   end
   
 end
