b62ac5f4d31dfee6578789c43ec64259084ed084
Henry <henryw2@illinois.edu>
2020-03-17 16:19:50 -0500

    require master key for rails 5.2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  # edited by me (3/17/2020)
+  config.require_master_key = true
 
   # Code is not reloaded between requests.
   config.cache_classes = true
